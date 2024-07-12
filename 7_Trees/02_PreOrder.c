b5b6c21d82d3c935a51ebe2b67719ee7310d8490
Himanshi <himanshidosarka@gmail.com>
2024-07-12 21:15:12 +0530

    commit

---
+++ 7_Trees/02_PreOrder.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+struct node* create(int data){
+    struct node *n;
+    n = (struct node *)malloc(sizeof(struct node));
+    n->data = data;
+    n->left = NULL;
+    n->right = NULL;
+    return n;
+}
+
+void preorder(struct node* root){
+    if(root != NULL){
+        printf("%d ", root->data);
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
+int main(){
+    struct node *p = create(4);
+    struct node *p1 = create(1);
+    struct node *p2 = create(6);
+    struct node *p3 = create(5);
+    struct node *p4 = create(2);
+    
+    p->left = p1;
+    p->right = p2;
+    p1->left = p3;
+    p1->right = p4;
+
+    preorder(p);
+    return 0;
+}
